960813c7c989b4f3deff9342317652faeb8450cb
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-07-05 10:17:24 +0300

    Function added for addTask

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,12 @@
 const startTimerBtn = document.getElementById("start-timer-btn");
 const resetTimerBtn = document.getElementById("reset-timer-btn");
 const addTaskBtn = document.getElementById("add-task-btn");
+
+addTaskBtn.addEventListener("click", () => addTask());
+
+function addTask() {
+  const taskRow = document.createElement("div");
+  const text = document.createElement("input");
+  text.type = "text";
+  text.placeholder = "Enter Value";
+}
