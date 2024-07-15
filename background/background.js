dd7affa3665cbe984b79332263077264489d6017
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-07-15 21:37:02 +0300

    Comment Lines Added

--- background/background.js
+++ background/background.js
@@ -1,23 +1,31 @@
+// Create an alarm named "focusFlow" that triggers every second (1/60 minutes).
 chrome.alarms.create("focusFlow", {
   periodInMinutes: 1 / 60,
 });
 
+// Add a listener for the alarm event.
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "focusFlow") {
+    // Check if the triggered alarm is "focusFlow".
     chrome.storage.local.get(["timer", "isRunning"], (res) => {
+      // Get the current timer and running status from local storage.
       if (res.isRunning) {
-        let timer = res.timer + 1;
+        // If the timer is running.
+        let timer = res.timer + 1; // Increment the timer by 1 second.
         let isRunning = true;
 
         if (timer >= 60 * 25) {
+          // Check if 25 minutes (1500 seconds) have passed.
           self.registration.showNotification("Focus Flow", {
+            // Show a notification.
             body: "25 minutes",
             icon: "icon.png",
           });
-          timer = 0;
-          isRunning = false;
+          timer = 0; // Reset the timer.
+          isRunning = false; // Stop the timer.
         }
 
+        // Update the local storage with the new timer and running status.
         chrome.storage.local.set({
           timer,
           isRunning,
@@ -27,9 +35,10 @@ chrome.alarms.onAlarm.addListener((alarm) => {
   }
 });
 
+// Initialize the timer and running status in local storage if they are not already set.
 chrome.storage.local.get(["timer", "isRunning"], (res) => {
   chrome.storage.local.set({
-    timer: "timer" in res ? res.timer : 0,
-    isRunning: "isRunning" in res ? res.isRunning : false,
+    timer: "timer" in res ? res.timer : 0, // Set timer to 0 if not already set.
+    isRunning: "isRunning" in res ? res.isRunning : false, // Set isRunning to false if not already set.
   });
 });
