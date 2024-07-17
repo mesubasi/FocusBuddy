ba072137dcbd25df8e6e362f46cc55c86fbf30e7
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-07-17 22:43:39 +0300

    Event Edits

--- options/options.js
+++ options/options.js
@@ -1,3 +1,8 @@
 const timeOption = document.getElementById("time-option");
 
-text.addEventListener("change", (event) => {});
+text.addEventListener("change", (event) => {
+  const val = event.target.value;
+  if (val < 1 || val > 60) {
+    timeOption.value = 25;
+  }
+});
