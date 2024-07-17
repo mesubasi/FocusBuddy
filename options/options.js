9046856df941d9f81993a6db9048463257941ba9
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-07-17 22:45:03 +0300

    New Event Added

--- options/options.js
+++ options/options.js
@@ -1,6 +1,6 @@
 const timeOption = document.getElementById("time-option");
 
-text.addEventListener("change", (event) => {
+timeOption.addEventListener("change", (event) => {
   const val = event.target.value;
   if (val < 1 || val > 60) {
     timeOption.value = 25;
@@ -8,3 +8,4 @@ text.addEventListener("change", (event) => {
 });
 
 const saveBtn = document.getElementById("save-btn");
+saveBtn.addEventListener("click", (event) => {});
